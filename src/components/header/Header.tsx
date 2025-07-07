031b5c86bd017a7ab168769efac78b761713199b
jouaspiazu <joeaspiazu@hotmail.com>
2025-07-07 14:14:17 -0500

    Ui improvements for responsive devices

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,14 +4,8 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <div className="header">
-        <h2>
-          <Link to="/">Ecuador Travel</Link>
-        </h2>
-        <div className="second-header">
-            
-                <h2>Ven y descubre Ecuador! 🇪🇨</h2>
-            
-        </div>
+        <h2><Link to="/">Ecuador Travel</Link></h2>
+        <h2 className='second-header'>Ven y descubre Ecuador! 🇪🇨</h2>
     </div>
   )
 }
