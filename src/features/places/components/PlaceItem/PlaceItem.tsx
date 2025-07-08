76a47dfd223efc4152cae0de26c358466d564bf2
jouaspiazu <joeaspiazu@hotmail.com>
2025-07-08 12:31:07 -0500

    Fixed one parameter in placeItem

--- src/features/places/components/PlaceItem/PlaceItem.tsx
+++ src/features/places/components/PlaceItem/PlaceItem.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 import { Place } from "../../types/PlaceType";
 import "./PlaceItem.scss";
 
-export default function PlaceItem({place, onDelete, isDetail}: {place: Place, 
-    onDelete: (placeId: string) => void, isDetail: boolean}) {
+export default function PlaceItem({place, onDelete}: {place: Place, 
+    onDelete: (placeId: string) => void}) {
 
     return (
         <> 
@@ -33,16 +33,17 @@ export default function PlaceItem({place, onDelete, isDetail}: {place: Place,
                 </Link>
                 <span>{place.city}</span>
             </div>
-            { isDetail && 
-                <div className="actions">
-                    <button className="delete" onClick={() => onDelete(place._id ? place._id : '')}>
-                        Eliminar
-                    </button>
-                    <button className="update">
-                        <Link to={`/addplace/${place._id}`}>Actualizar</Link>
-                    </button>
-                </div>
-            }
+            {/* Removed temporary */}
+            {/* Create a signin implementation to edit places */}
+            {/* <div className="actions">
+                <button className="delete" onClick={() => onDelete(place._id ? place._id : '')}>
+                    Eliminar
+                </button>
+                <button className="update">
+                    <Link to={`/addplace/${place._id}`}>Actualizar</Link>
+                </button>
+            </div> */}
+            
             </div>
         </>
         
