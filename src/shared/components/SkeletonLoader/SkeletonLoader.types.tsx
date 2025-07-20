b4247c2521d1ad835f8cab33e7fc272e499ecdb6
jouaspiazu <joeaspiazu@hotmail.com>
2025-07-20 17:17:53 -0500

    Refactor skeleton component in shared and changed in PlaceItemSkeleton

---
+++ src/shared/components/SkeletonLoader/SkeletonLoader.types.tsx
@@ -0,0 +1,8 @@
+export interface BaseSkeletonProps {
+  count?: number;
+  height?: number | string;
+  width?: number | string;
+  borderRadius?: number | string;
+  className?: string;
+  circle?: boolean;
+}
