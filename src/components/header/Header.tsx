5e9584db1c6fd182db6393ac822e50f8ce008b27
jouaspiazu <joeaspiazu@hotmail.com>
2025-08-03 15:00:30 -0500

    Added link to admin/flags

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,7 +5,9 @@ export default function Header() {
   return (
     <div className="header">
         <h2><Link to="/">Ecuador 🇪🇨 Travel</Link></h2>
-        <h2 className='second-header'>Ven y descubre Ecuador! 🇪🇨</h2>
+        <h2 className='second-header'>
+          <Link to={"/admin/flags"}> Ven y descubre Ecuador! 🇪🇨</Link>
+          </h2>
     </div>
   )
 }
